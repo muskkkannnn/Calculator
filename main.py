3d831814ae946cd1b68d602c0e5f21352a55d0b3
CodeSandbox <support@codesandbox.io>
2024-09-28 02:14:48 +0000

    Sandbox snapshot commit

--- main.py
+++ main.py
@@ -11,8 +11,15 @@ def div(n1, n2):
     return n1 / n2
 
 operations = {
-    "+" : add()
-    "-" : sub()
-    "*" : multiply()
-    "/" : div()
+    "+" : add,
+    "-" : sub,
+    "*" : multiply,
+    "/" : div
 }
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+print("Choose an operation: ")
+
+for symbol in operations:
+    print(symbol)
