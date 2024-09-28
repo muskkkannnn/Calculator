0ee84674ec4ab1a9a7bb32d2ab5fbefbfa04e393
CodeSandbox <support@codesandbox.io>
2024-09-28 01:50:58 +0000

    Sandbox snapshot commit

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("Hello CodeSandbox!")
+def add(n1, n2):
+    return n1 + n2
+
+def sub(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def div(n1, n2):
+    return n1 / n2
+
+operations = {
+    "+" : add()
+    "-" : sub()
+    "*" : multiply()
+    "/" : div()
+}
