cd75e81a63a11b1bc57ab235f048bf73379e43db
Muskan Srivastava <110267124+muskkkannnn@users.noreply.github.com>
2024-09-30 07:42:36 +0000

    update version 1

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 #version 1
+from art import logo
+print(logo)
 
 def add(n1, n2):
     return n1 + n2
@@ -19,9 +21,41 @@ operations = {
     "/" : div
 }
 
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
-print("Choose an operation: ")
+num1 = int(input("What's the first number?: "))
 
 for symbol in operations:
     print(symbol)
+
+operation_symbol = input("Pick an operation: ")
+calculation_function = operations[operation_symbol]
+
+num2 = int(input("What's the second number?: "))
+
+answer = calculation_function(num1, num2)
+print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+#version 2
+
+# def calculator():
+#     num1 = float(input("What's the first number?: "))
+
+#     for symbol in operations:
+#         print(symbol)
+
+#     should_continue = True
+#     while should_continue:
+#         operation_symbol = input("Pick an operation: ")
+#         calculation_function = operations[operation_symbol]
+
+#         num2 = float(input("What's the second number?: "))
+
+#         answer = calculation_function(num1, num2)
+#         print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+#         if input("Press 'y' to continue or Press 'n' to start a new calculation: ") == "y":
+#             num1 = answer
+#         else:
+#             should_continue = False
+#             calculator()
+
+# calculator()
